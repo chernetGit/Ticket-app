46559eab40f2dd2f354821b8b879acd583bb8193
chernetGit <chernettibebuyigzaw@gmail.com>
2023-05-25 15:41:06 +0300

    Connecting feontend and backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,20 +3,20 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
-// @desc  Register a new user
-// @route /api/users
-// @access Public
+
+// @desc    Register a new user
+// @route   /api/users
+// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
-  //validation
+  // Validation
   if (!name || !email || !password) {
-    // return res.status(400).json({ message: 'Please include all fields' })
     res.status(400)
     throw new Error('Please include all fields')
   }
 
-  // Find if user already exist
+  // Find if user already exists
   const userExists = await User.findOne({ email })
 
   if (userExists) {
@@ -24,7 +24,7 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('User already exists')
   }
 
-  // Hash Password
+  // Hash password
   const salt = await bcrypt.genSalt(10)
   const hashedPassword = await bcrypt.hash(password, salt)
 
@@ -46,20 +46,17 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new error('Invalid user data')
   }
-
-  // res.send('Register Route')
 })
 
-// @desc  Login a user
-// @route /api/users/login
-// @access Public
+// @desc    Login a user
+// @route   /api/users/login
+// @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
   const user = await User.findOne({ email })
 
-  // res.send('Login Route')
-  //Check user and passwords match my password is (12345678)
+  // Check user and passwords match
   if (user && (await bcrypt.compare(password, user.password))) {
     res.status(200).json({
       _id: user._id,
@@ -73,11 +70,10 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc get current user
-// @route /api/users/me
-// @access Private
+// @desc    Get current user
+// @route   /api/users/me
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  // res.send('me')
   const user = {
     id: req.user._id,
     email: req.user.email,
@@ -86,7 +82,7 @@ const getMe = asyncHandler(async (req, res) => {
   res.status(200).json(user)
 })
 
-//Generate Token
+// Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,17 +4,24 @@ const User = require('../models/userModel')
 
 const protect = asyncHandler(async (req, res, next) => {
   let token
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      //Get token from header
+      // Get token from header
       token = req.headers.authorization.split(' ')[1]
-      //Verify token
+      // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
-      //Get user from token
+      // Get user from token
       req.user = await User.findById(decoded.id).select('-password')
+      // NOTE: We need to check if a user was found
+      // https://www.udemy.com/course/react-front-to-back-2022/learn/lecture/30591026#questions/17843570
+      if (!req.user) {
+        res.status(401)
+        throw new Error('Not authirised')
+      }
 
       next()
     } catch (error) {
@@ -25,7 +32,6 @@ const protect = asyncHandler(async (req, res, next) => {
   }
 
   if (!token) {
-    console.log(error)
     res.status(401)
     throw new Error('Not authorized')
   }
